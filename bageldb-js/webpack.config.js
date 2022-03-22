548e4fecc6de51659c5153a9d1f0663cd6b0d405
nallon <90545427+nallon@users.noreply.github.com>
2022-03-22 11:30:02 +0200

    Add: potential fix for node env in new alpha

--- bageldb-js/webpack.config.js
+++ bageldb-js/webpack.config.js
@@ -2,6 +2,7 @@ const path = require("path");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 // const HtmlWebpackPlugin = require("html-webpack-plugin");
 const nodeExternals = require("webpack-node-externals");
+const NodePolyfillPlugin = require("node-polyfill-webpack-plugin");
 
 const generalConfig = {
   devtool: "inline-source-map",
@@ -40,6 +41,7 @@ const generalConfig = {
       cleanStaleWebpackAssets: false,
       cleanOnceBeforeBuildPatterns: [path.resolve(__dirname, "./dist")],
     }),
+    new NodePolyfillPlugin(),
     // new HtmlWebpackPlugin({
     //   title: "bageldb-js",
     // }),
