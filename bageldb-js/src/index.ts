342024ed49a4a9d356e64ee231ebf76d56d33548
nallon <90545427+nallon@users.noreply.github.com>
2022-03-26 13:23:31 +0300

    config changes and bump version

--- bageldb-js/src/index.ts
+++ bageldb-js/src/index.ts
@@ -5,30 +5,6 @@ import {
   BagelUsersRequest,
   axios,
 } from './common';
-if (typeof document !== 'undefined') {
-  console.log('bagelDB is running in a browser');
-} else if (typeof globalThis?.navigator !== 'undefined' && globalThis?.navigator?.product === 'ReactNative') {
-  // I'm in react-native
-} else {
-  // I'm in node js
-  if (!globalThis?.EventSource) {
-    try {
-      // (async ()=> {
-
-      // eslint-disable-next-line @typescript-eslint/no-var-requires
-      // const EventSource = require('eventsource');
-      // const EventSource = await import('eventsource');
-      // globalThis.EventSource = EventSource.default as any;
-      // })();
-
-      // eslint-disable-next-line @typescript-eslint/no-implied-eval
-    //   const setIt = new Function('try {return globalThis.EventSource = require("eventsource");}catch(e){return false;}');
-    //   setIt();
-    } catch (error) {
-      console.warn('if you are running bagelDB in nodejs environment you might need the `eventsource` npm package for live data to function');
-    }
-  }
-}
 
 export default class Bagel {
   [x: string]: any;
