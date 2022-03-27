341a30fa260f14b1a0cd699d80df9e168e4fe50b
nevehallon <nevehallon@gmail.com>
2022-03-27 10:31:45 +0300

    refactor, change config and bump dev version

--- bageldb-js/src/server/index.ts
+++ bageldb-js/src/server/index.ts
@@ -3,6 +3,11 @@ import { BagelDBRequest, BagelMetaRequest, BagelUsersRequest } from '../common';
 // globalThis.EventSource = require('eventsource');
 import EventSource from 'eventsource';
 globalThis.EventSource = EventSource as unknown as typeof globalThis.EventSource;
+import FallbackStorage from '../fbStorage';
+
+if (!globalThis?.localStorage) {
+  globalThis.localStorage = new FallbackStorage({});
+}
 export {
   BagelUsersRequest,
   BagelDBRequest,
