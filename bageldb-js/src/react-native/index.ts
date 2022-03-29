08892b07a0df5232c5c4f1cc5cfc1cd4ef6cb383
nevehallon <nevehallon@gmail.com>
2022-03-29 17:01:10 +0300

    change config, bump dev version, and refactor

---
+++ bageldb-js/src/react-native/index.ts
@@ -0,0 +1,14 @@
+import BagelDB from '../index';
+import { BagelDBRequest, BagelMetaRequest, BagelUsersRequest } from '../common';
+import FallbackStorage from '../fbStorage';
+import FormData from 'form-data';
+
+if (!globalThis?.localStorage) {
+  globalThis.localStorage = new FallbackStorage({});
+}
+if (!globalThis?.FormData) {
+  (globalThis as any).FormData = FormData;
+}
+
+export { BagelUsersRequest, BagelDBRequest, BagelMetaRequest, BagelDB };
+export default BagelDB;
