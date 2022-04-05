0115c92642acbf68d243ea2d44e702c37e75c383
nallon <90545427+nallon@users.noreply.github.com>
2022-04-05 17:33:20 +0300

    bump dev version

--- bageldb-js/webpack.config.cjs
+++ bageldb-js/webpack.config.cjs
@@ -146,7 +146,7 @@ const esmConfig = {
   },
   // target: "node",
   // externalsPresets: { node: true }, // in order to ignore built-in modules like path, fs, etc.
-  // externals: [nodeExternals()], // in order to ignore all modules in node_modules folder
+  externals: [nodeExternals()], // in order to ignore all modules in node_modules folder
   //  plugins: [
   //   new CleanWebpackPlugin({
   //     cleanStaleWebpackAssets: false,
