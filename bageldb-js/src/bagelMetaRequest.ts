8be6e54dbfda37fcbef6f5eb44df63a3e603e32e
nallon <90545427+nallon@users.noreply.github.com>
2022-07-20 11:55:23 +0300

    Add: better typing

--- bageldb-js/src/bagelMetaRequest.ts
+++ bageldb-js/src/bagelMetaRequest.ts
@@ -1,12 +1,22 @@
 import { AxiosPromise } from 'axios';
 import { baseEndpoint } from './common';
-import  BagelDBRequest from './bagelDBRequest';
+import BagelDBRequest from './bagelDBRequest';
 import { structArgs } from './interfaces';
 export default class BagelMetaRequest extends BagelDBRequest {
   constructor({ instance, collectionID }: structArgs) {
     super({ instance, collectionID });
   }
 
+  /**
+   * @summary
+   * Retrieve the schema of a collection.
+   * This enables implementing things like dynamic forms or other dynamic pages, which rely on the schema to display different page components.
+   * Schema will be retrieved for the parent collection, and will contain the schema for all nested collections inside the parent collection.
+   * @example Request:
+   * const {data: schema} = await db.schema("chat").get();
+   * @returns {AxiosPromise} A Promise with the schema of the collection.
+   * @see Docs {@link https://docs.bageldb.com/meta-api/#get-schema}
+   */
   get(): AxiosPromise {
     return new Promise((resolve, reject) => {
       const url = `${baseEndpoint}/collection/${this.collectionID}/schema`;
