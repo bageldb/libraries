e7ee9aeb6770a79869e75488d0a0a3d7af42ecd5
nevehallon <nevehallon@gmail.com>
2022-08-10 14:38:42 +0300

    Add: storing user is in storage after decoding from JWT

--- bageldb-js/src/common.ts
+++ bageldb-js/src/common.ts
@@ -15,5 +15,32 @@ const getExpires = (expires_in: number): string => {
   const expires = new Date(expires_in);
   return `${expires.setSeconds(expires.getSeconds() + expires_in)}`;
 };
+
+/**
+ * Returns a JS object representation of a Javascript Web Token from its common encoded
+ * string form.
+ *
+ * @template T the expected shape of the parsed token
+ * @param {string} token a Javascript Web Token in base64 encoded, `.` separated form
+ * @returns {(T | undefined)} an object-representation of the token
+ * or undefined if parsing failed
+ */
+const getParsedJwt = <T extends object = { [k: string]: string | number }>(
+  token: string,
+): T | undefined => {
+  try {
+    return JSON.parse(atob(token.split('.')[1]));
+  } catch {
+    return undefined;
+  }
+};
 export type { AxiosInstance, AxiosPromise, AxiosResponse };
-export { axios, baseEndpoint, liveEndpoint, isReactNative, isNode, getExpires };
+export {
+  axios,
+  baseEndpoint,
+  liveEndpoint,
+  isReactNative,
+  isNode,
+  getExpires,
+  getParsedJwt,
+};
