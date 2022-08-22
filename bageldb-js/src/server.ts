1b9c7b5ec842ce51e92d69fc8b6cdee41ff4d432
nevehallon <nevehallon@gmail.com>
2022-08-22 14:53:00 +0300

    potentential fix for fallback storage

--- bageldb-js/src/server.ts
+++ bageldb-js/src/server.ts
@@ -8,11 +8,7 @@ globalThis.EventSource =
   globalThis?.EventSource ||
   (EventSource as unknown as typeof globalThis.EventSource);
 
-import FallbackStorage from './fbStorage';
 import FormData from 'form-data';
-
-if (!globalThis?.localStorage)
-  globalThis.localStorage = new FallbackStorage({});
 
 if (!globalThis?.FormData) (globalThis as any).FormData = FormData;
 
