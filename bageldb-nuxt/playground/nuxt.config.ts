31a88e7a56393df3b2d78cf230e552105b214cd3
Neveh A <nevehallon@gmail.com>
2023-10-30 17:46:28 +0200

    Update nuxt.config.ts

--- bageldb-nuxt/playground/nuxt.config.ts
+++ bageldb-nuxt/playground/nuxt.config.ts
@@ -1,16 +1,13 @@
-import { fileURLToPath } from "url";
-
 export default defineNuxtConfig({
   // alias: {
   //   '@bageldb/bageldb-nuxt': import.meta.env.NUXT_ENV_BAGEL_NUXT_ALIAS
   //     ? '@bageldb/bageldb-nuxt/dist'
   //     : fileURLToPath(new URL('../src', import.meta.url))
-
   // },
+  
   modules: [
     [
-      // '../src/module',
-      '@bageldb/bageldb-nuxt',
+      '../src/module',
       { token: process.env.NUXT_ENV_BAGEL_TOKEN, exposePublicClient: true }
     ]
   ],
